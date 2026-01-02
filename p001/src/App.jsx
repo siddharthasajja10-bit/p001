5b3834c5378ad5e55add8ac8f48bdc3ae48a8d4d
siddharthasajja10-bit <siddharthasajja10@gmail.com>
2026-01-02 10:54:25 +0530

    v1

---
+++ p001/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App(){
+  return(
+    <div className='app'>
+      <p>Name: s .siddhartha</p>
+     <p>Program: B.Tech-CSE</p> 
+     <p> Section: S03</p>
+      <p>Course: Front End Development Frameworks and UI Engineering</p>
+          </div>
+  );
+}
+export default App;
